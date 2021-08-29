5a625f0333534def552d87cc6afe7ff8d0f2d7cc
root <root@CoreOS1.local>
2021-08-29 10:55:25 -0400

    tailscaled and tailscale up combined into single entrypoint

--- start.sh
+++ start.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-tailscaled
-#while [ 1 ]
-#do
-#    sleep 0.1
-#done
-echo Tailscale started
+command='/usr/bin/tailscale up'
+
+#advertise-routes
+if [[ -z "${ROUTE}" ]]; then
+  echo "No Route set..."
+else
+  command="${command} --advertise-routes ${ROUTE}"
+fi
+
+if [[ -z "${AUTHKEY}" ]]; then
+  echo "No Auth Key Specified"
+else
+  command="${command} --authkey ${AUTHKEY}"
+fi
+
+
+sleep 5 && eval $command 2>&1 &
+/usr/bin/tailscaled
